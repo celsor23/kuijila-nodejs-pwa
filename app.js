9a9428f88b36b7dfb62d1767c6f2aad1e254cfe9
Celso Ribeiro <celsor23@gmail.com>
2021-11-26 14:59:14 -0800

    add js folder, install node path module and implent it

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
-app.set("views", "views");
+app.set("views", path.join( __dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(path.join( __dirname, "public")));
 
 app.get("/", (req, res, next) => {
   res.render("index");
