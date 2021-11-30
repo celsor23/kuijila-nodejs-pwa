84e09e898d3b59edf57c1c228ff48e8f650b8400
Celso Ribeiro <celsor23@gmail.com>
2021-11-30 14:32:11 -0800

    change app.listen from '8080' to port

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.all("*", (req, res, next) => {
 
 const port = process.env.PORT || 8080;
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
