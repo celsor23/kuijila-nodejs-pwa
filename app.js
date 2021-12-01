066270d927ca33f585f5665c8ec643cadeb3d7c3
Celso Ribeiro <celsor23@gmail.com>
2021-11-30 16:20:02 -0800

    install firebase-admin, initialize app with service account, and pass firebaseApp to req object

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { initializeApp, cert } = require("firebase-admin/app");
 const helmet = require("helmet");
 const compression = require("compression");
 const path = require("path");
@@ -6,12 +7,23 @@ require("dotenv").config();
 
 const app = express();
 
+const serviceAccount = require("./firebase/kuijila-firebase-adminsdk-x7se2-d5d889f59e.json");
+
+const firebaseApp = initializeApp({
+  credential: cert(serviceAccount)
+});
+
 const homepageRoutes = require("./routes/index");
 const entrarRoutes = require("./routes/entrar");
 const inscrevaseRoutes = require("./routes/inscreva-se");
 
 app.set("views", path.join( __dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+  req.firebaseApp = firebaseApp;
+  next();
+});
 
 app.use(helmet());
 app.use(compression());
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
 exports.getHomepage = (req, res, next) => {
+  console.log(req.firebaseApp);
   res.render("index", {pathname: req.baseUrl});
 };
