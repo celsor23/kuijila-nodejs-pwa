a6e51f4b0d55405b3007cc35c21465bc7b81acbf
Celso Ribeiro <celsor23@gmail.com>
2021-12-05 14:40:50 -0800

    implement firebase app initialization script correctly with the required Content-Security-Policy

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,12 @@
+const { hash } = require("../helpers/hash-generator");
+
 exports.getHomepage = (req, res, next) => {
+  const generatedHash = hash("getHomepageFirebaseJsSDKInlineScript");
+  const cspHeaders = {
+    "Content-Security-Policy": "script-src 'self' 'unsafe-inline';",
+    "Content-Security-Policy": `script-src-elem 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com/ 'nonce-64c84785f7jds78' ${generatedHash};`
+  }
   console.log(req.firebaseApp);
   console.log(`${req.protocol}://${req.hostname}:8080/${req.baseUrl}`);
-  res.render("index", {pathname: req.baseUrl});
+  res.set(cspHeaders).render("index", {pathname: req.baseUrl});
 };
--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCUr261XPvHeRBb72mTSG9UKJAQ7wZLnVc",
-  authDomain: "kuijila.firebaseapp.com",
-  projectId: "kuijila",
-  storageBucket: "kuijila.appspot.com",
-  messagingSenderId: "107332910029",
-  appId: "1:107332910029:web:7cfb8ffe1d650331666284",
-  measurementId: "G-ZQ5BHS4LVG"
-};
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-
 const signupContent = document.querySelector(".content__sign-up");
 const qAndAContent = document.querySelector(".content__q-and-a");
 const enjoyContent = document.querySelector(".content__enjoy");
@@ -40,3 +18,6 @@ enjoyButton.addEventListener("click", () => {
   qAndAContent.classList.remove("block");
   enjoyContent.classList.add("block");
 });
+
+console.log(window.app);
+console.log(window.auth);
