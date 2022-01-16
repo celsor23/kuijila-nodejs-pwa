c172afe0b674cd010f5eb22354bfb0ade9366f35
Celso Ribeiro <celsor23@gmail.com>
2022-01-15 19:47:12 -0800

    add validate, constraints and error messages to users schema to better validate it with the correct input

--- models/users.js
+++ models/users.js
@@ -5,21 +5,40 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 5
+    required: [true, 'Name required!'],
+    minlength: [5, 'Name must be at least 5 characters long!']
   },
   email: {
     type: String,
-    required: true
+    lowercase: true,
+    validate: {
+      validator: function (v) {
+        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email address!`
+    },
+    required: [true, 'Email address required']
   },
   password: {
     type: String,
-    required: true,
-    minlength: 7
+    validate: {
+      validator: function(v) {
+        return /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,16}$/.test(v);
+      },
+      message: props => `Password must have at least one number and a special character (!, @, #, $, %, ^, &, *), and be between 7-16 characters long. ${props.value} is not a valid password!`
+    },
+    required: [true, 'Password is required!'],
+    minlength: [7, 'Password must be at least 7 characters long!'],
   },
   phoneNumber: {
     type: String,
-    required: true
+    validate: {
+      validator: function(v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
   },
   accountType: {
     type: String,
