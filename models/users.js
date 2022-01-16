d6cc021eeeda3397ac79981fc8b93a2597d486b7
Celso Ribeiro <celsor23@gmail.com>
2022-01-15 20:35:01 -0800

    refactor user schema validator regex to validate international phone numbers with their country codes

--- models/users.js
+++ models/users.js
@@ -34,9 +34,9 @@ const userSchema = new Schema({
     type: String,
     validate: {
       validator: function(v) {
-        return /\d{3}-\d{3}-\d{4}/.test(v);
+        return /^\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/.test(v);
       },
-      message: props => `${props.value} is not a valid phone number!`
+      message: props => `${props.value} is not a valid phone number (+xxxxxxx...)!`
     },
     required: [true, 'User phone number required']
   },
