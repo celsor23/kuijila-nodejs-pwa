d4d3fb24846d0ccc084699a2956b6f19821518b8
Celso Ribeiro <celsor23@gmail.com>
2022-01-26 14:54:12 -0800

    add express.json() middleware

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set("view engine", "html");
 app.use(helmet());
 app.use(compression());
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static(path.join( __dirname, "public")));
 
 app.use(setCORSAndCSPHeadersMiddleware);
